0a1f94e1156ec7c0f035a18e9ba35b50eda03e45
Nazakat-Ejazz <nazakatejaz.work@gmail.com>
2025-01-07 14:51:22 +0500

    Added a server action onAuthenticateUser for auth

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -6,14 +6,73 @@ import { client } from '@/lib/prisma';
 export const onAuthenticateUser = async () => {
   try {
     const user = await currentUser();
+    console.log('From clerk: currentUser : ', currentUser);
     if (!user) {
       return { status: 403 };
     }
 
-    const existingUser = await client.user.findUnique({
+    const userExists = await client.user.findUnique({
       where: {
-        clerkId: user?._id,
+        clerkId: user.id,
+      },
+      include: {
+        workSpace: {
+          where: {
+            User: {
+              clerkId: user.id,
+            },
+          },
+        },
       },
     });
-  } catch (error) {}
+    // user already exists return it
+    if (userExists) {
+      return { status: 200, user: userExists };
+    }
+    // else case create user
+    const newUser = await client.user.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        firstname: user.firstName,
+        lastname: user.lastName,
+        image: user.imageUrl,
+        studio: {
+          create: {},
+        },
+        subscription: {
+          create: {},
+        },
+        workSpace: {
+          create: {
+            name: `${user.firstName}'s Workspace`,
+            type: 'PERSONAL',
+          },
+        },
+      },
+      include: {
+        workSpace: {
+          where: {
+            User: {
+              clerkId: user.id,
+            },
+          },
+        },
+        subscription: {
+          select: {
+            plan: true,
+          },
+        },
+      },
+    });
+    if (newUser) {
+      return {
+        status: 201,
+        user: newUser,
+      };
+    }
+    return { status: 400 };
+  } catch (error) {
+    return { status: 500 };
+  }
 };
