b315a3915cdeb70d95d903181ee7d5698be28463
Nazakat-Ejazz <nazakatejaz.work@gmail.com>
2025-01-13 05:21:48 +0500

    Fix: fixed theme import bug

--- src/components/theme/index.tsx
+++ src/components/theme/index.tsx
@@ -1,10 +1,11 @@
 'use client';
+
 import * as React from 'react';
-import { ThemeProvider as NextThemeProvider } from 'next-themes';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 export function ThemeProvider({
   children,
   ...props
-}: React.ComponentProps<typeof NextThemeProvider>) {
-  return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
+}: React.ComponentProps<typeof NextThemesProvider>) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
