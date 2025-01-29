d200ffb9241de1de293edb6f8196edf4ea7455fc
Nazakat Ejaz <nazakatejaz.work@gmail.com>
2025-01-29 17:49:51 +0500

    Added custom query for fetching workspaces

--- src/components/global/sidebar/index.tsx
+++ src/components/global/sidebar/index.tsx
@@ -1,16 +1,57 @@
+'use client';
 import React from 'react';
+import Image from 'next/image';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Separator } from '@/components/ui/separator';
+import { useRouter } from 'next/navigation';
 
 type Props = {
   activeWorkspaceId: string;
 };
 
-const Sidebar = ({ activeWorkspaceId: string }: Props) => {
-  console.log('Sidebar is being rendered!');
+const Sidebar = ({ activeWorkspaceId }: Props) => {
+  const router = useRouter();
+
+  // method to handle workspace change
+  const handleWorkSpaceChanger = (val: string) => {
+    router.push(`/workspace/${val}`);
+  };
+
   return (
-    <div className='bg-[#111111] flex-none relative p-4 h-full w-[250px] flex flex-col items-center gap-4 '>
+    <div className='bg-[#111111] flex-none relative p-4 h-full w-[250px] flex flex-col items-center gap-4 text-white '>
       <div className='bg-[#111111] p-4 gap-2 justify-center items-center mb-1 absolute top-0 left-0 right-0'>
-        {' '}
-        Sidebarrrr!
+        {/* <!-- brand info --> */}
+        <div className='flex item-center justify-center gap-4'>
+          <Image
+            src={'/opal-logo.svg'}
+            alt='opal logo'
+            width={40}
+            height={40}
+          />
+          <h1 className='text3xl text-white'>Opal</h1>
+        </div>
+        {/* workspace select dropdown */}
+        <Select
+          defaultValue={activeWorkspaceId}
+          onValueChange={handleWorkSpaceChanger}
+        >
+          <SelectTrigger className='mt-16 text-neutral-400 bg-transparent'>
+            <SelectValue>Switch Workspace</SelectValue>
+          </SelectTrigger>
+          <SelectContent className='bg-blur-xl bg-[#111111] bg-slate-500'>
+            <SelectGroup>
+              <SelectLabel>Workspaces</SelectLabel>
+              <Separator />
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
     </div>
   );
---
+++ src/hooks/useQueryData.ts
@@ -0,0 +1,22 @@
+import {
+  Enabled,
+  QueryFunction,
+  QueryKey,
+  useQuery,
+} from '@tanstack/react-query';
+
+export const useQueryData = (
+  queryKey: QueryKey,
+  queryFn: QueryFunction,
+  enabled: Enabled
+) => {
+  const { data, isPending, isFetching, isFetched, isError, refetch } = useQuery(
+    {
+      queryKey,
+      queryFn,
+      enabled,
+    }
+  );
+
+  return { data, isPending, isFetched, isFetching, isError, refetch };
+};
